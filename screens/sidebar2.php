735a776deb72e942dcb6b9baae03942779772651
palolik <azizulalamprottoy@gmail.com>
2024-01-16 00:32:56 +0600

    final

--- screens/sidebar2.php
+++ screens/sidebar2.php
@@ -118,11 +118,14 @@ function closeForm8() {
          <div>
          <form action='test2.php' method="post">
         <input style="display:none;" type="email" name="emai" value="ansarulalam1963@gmail.com">
-        <input style="display:none;" type="text" name="subject" value="Mainteinence request ">
-        <input style="display:none" value="<?php require_once("../config/database.php");$resultw = mysqli_query($mysqli, "SELECT * FROM employees where employeeid=180011266"); while($res = mysqli_fetch_assoc($resultw)) { echo $res['email'] ;} ?>" name="email">
 
-        <input style="display:none" value="180011266" name="employeeid">
-        <textarea style="margin:20px;padding:10px;resize:none;color:black;font-size:20px;width:-webkit-fill-available;height:300px;background-color:white;" type="text" name="request" value=""><?php require_once("../config/database.php");$resultw = mysqli_query($mysqli, "SELECT * FROM employees where employeeid=180011266"); while($res = mysqli_fetch_assoc($resultw)) { echo  $res['email']  ;} ?><?php echo date("F jS, Y, h:i A"); ?>
+        <input style="display:none;" type="text" name="subject" value="Mainteinence request ">
+
+        <input style="display:none" value="<?php require_once("../config/database.php");$resultw = mysqli_query($mysqli, "SELECT * FROM employees where employeeid=$eid"); while($res = mysqli_fetch_assoc($resultw)) { echo $res['email'] ;} ?>" name="email">
+
+        <input style="display:none" value="<?php echo $eid; ?>" name="employeeid">
+
+        <textarea style="margin:20px;padding:10px;resize:none;color:black;font-size:20px;width:-webkit-fill-available;height:300px;background-color:white;" type="text" name="request" value=""><?php require_once("../config/database.php");$resultw = mysqli_query($mysqli, "SELECT * FROM employees where employeeid=$eid"); while($res = mysqli_fetch_assoc($resultw)) { echo  $res['email']  ;} ?><?php echo date("F jS, Y, h:i A"); ?>
             
     </textarea>
         <input style="display:none" name="date"  value="<?php echo date("F jS, Y, h:i A");  ?>" >
